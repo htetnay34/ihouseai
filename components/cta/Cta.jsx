a373e2b94d72a7182a831517ef6651d8bd19a415
htetnay34 <132404834+htetnay34@users.noreply.github.com>
2024-01-20 21:18:52 +0630

    Update Cta.jsx

--- components/cta/Cta.jsx
+++ components/cta/Cta.jsx
@@ -21,7 +21,7 @@ export default function Cta() {
                 href="/dashboard"
                 className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
               >
-                Commencer <span aria-hidden="true">&rarr;</span>
+                စတင်အသုံးပြုမယ် <span aria-hidden="true">&rarr;</span>
               </Link>
             </div>
           </div>
