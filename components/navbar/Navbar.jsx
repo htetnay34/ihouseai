f11576c76c11ce4ca8c2e48808f87c001e1e691d
htetnay34 <132404834+htetnay34@users.noreply.github.com>
2024-01-20 08:13:44 +0630

    Update Navbar.jsx

--- components/navbar/Navbar.jsx
+++ components/navbar/Navbar.jsx
@@ -88,7 +88,7 @@ export default function Navbar() {
             className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
             onClick={() => setMobileMenuOpen(true)}
           >
-            <span className="sr-only">Ouvrir le menu</span>
+            <span className="sr-only">Menu</span>
             <Bars3Icon className="h-6 w-6" aria-hidden="true" />
           </button>
         </div>
@@ -121,7 +121,7 @@ export default function Navbar() {
                 onClick={() => setMobileMenuOpen(false)}
                 className="ml-auto rounded-md px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
               >
-                Tableau de bord
+                Dashboard အခန်း
               </a>
             ) : (
               <Link
@@ -129,7 +129,7 @@ export default function Navbar() {
                 href="/auth/login"
                 className="ml-auto rounded-md px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
               >
-                Connexion
+                ဝင်မယ်
               </Link>
             )}
             <button
@@ -163,7 +163,7 @@ export default function Navbar() {
                       onClick={handleSignOut}
                       className="rounded-md  px-3 py-2 text-sm bg-gray-100 font-semibold text-black shadow-sm hover:bg-gray-300 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray-600"
                     >
-                      Deconnexion
+                      ထွက်မယ်
                     </button>
                   </div>
                 ) : (
@@ -174,7 +174,7 @@ export default function Navbar() {
                         href="/auth/sigin"
                         className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                       >
-                        Inscription
+                        အကောင့်ဖွင့်မယ်
                       </Link>
                     </div>
                   </div>
