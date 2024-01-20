472d5f061c6059c4f78d41418abc04bbf2ad1e62
htetnay34 <132404834+htetnay34@users.noreply.github.com>
2024-01-20 15:18:19 +0630

    Update dashboard.jsx

--- components/dashboardui/dashboard.jsx
+++ components/dashboardui/dashboard.jsx
@@ -396,7 +396,7 @@ export default function Dashboard() {
     },
     {
       id: 7,
-      name: "Salle de jeu",
+      name: "Gaming Room",
       translated: "Gaming Room",
       href: "#",
       initial: "Sj",
