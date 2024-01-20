34074d238554574958d306ca84c50ef4959161de
htetnay34 <132404834+htetnay34@users.noreply.github.com>
2024-01-20 20:01:22 +0630

    Update LoadingStore.jsx

--- components/LoadingStore.jsx
+++ components/LoadingStore.jsx
@@ -4,7 +4,7 @@ export default function LoadingStore() {
       <div className="flex flex-col items-center justify-center h-full w-full">
         <div className="   loadering">
           <span className="inline-flex items-center gap-x-0.5 rounded-md  px-2 py-1 text-2xl font-semibold text-gray-900">
-            CHARGEMENT EN COURS <span className="bullets">.</span>
+            ခဏစောင့်ပါ.. <span className="bullets">.</span>
           </span>
         </div>
         <div className="boxes">
