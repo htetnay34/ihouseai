a574cb3151703dad8fb0229bfbaac8732fdf0067
htetnay34 <132404834+htetnay34@users.noreply.github.com>
2024-01-21 20:23:34 +0630

    Update signin.jsx

--- components/auth/signin.jsx
+++ components/auth/signin.jsx
@@ -237,7 +237,7 @@ export default function Sigin() {
                   type="submit"
                   className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                 >
-                  ဆက်လက်
+                  ဝင်မယ်
                 </button>
               </div>
             </form>
